ca201b241951ab8f4df4e71b8ecbd47c2d23595b
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-25 17:58:11 +0600

    Bug fixes. Resolved 'Undefined' issue.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './Card.css';
-
-const suitInfo = {
-    h: '♥hearts',
-    d: '♦diams',
-    s: '♠spades',
-    c: '♣clubs'
-};
+import CardDeck from '../CardDeck/CardDeck'
 
 const Card = props => {
     return (
-        <div className={"Card Card-rank-" + props.rank + " Card-" + suitInfo[props.suit].slice(1)}>
+        <div className={"Card Card-rank-" + props.rank + " Card-" + CardDeck.suitInfo[props.suit].slice(1)}>
             <span className="Card-rank">{props.rank.toUpperCase()}</span>
-            <span className="Card-suit">{suitInfo[props.suit].slice(0,1)}</span>
+            <span className="Card-suit">{CardDeck.suitInfo[props.suit].slice(0,1)}</span>
         </div>
     );
 };
