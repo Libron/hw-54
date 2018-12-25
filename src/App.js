afec0194b33c8a6b4927cf1077a91b9ed54e0d27
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-25 18:33:08 +0600

    Added money state. Extra functionality.

--- src/App.js
+++ src/App.js
@@ -14,14 +14,51 @@ class App extends Component {
           {suit: "h", ranks: "j"},
           {suit: "h", ranks: "10"}
       ],
-      result: 'Royal Flush',
+      result: 'Royal Flush (test)',
       money: 0
   };
 
   shuffleCards = () => {
       let deck = new CardDeck().getCards(5);
       this.setState({fiveCards: deck, result: PokerHand.getOutcome(deck)});
-  }
+      this.updateAmount();
+  };
+
+  updateAmount = () => {
+      let currentMoney = this.state.money;
+
+      switch(this.state.result) {
+          case 'Royal Flush':
+              this.setState({money: currentMoney + 2000});
+              break;
+          case 'Straight Flush':
+              this.setState({money: currentMoney + 1200});
+              break;
+          case 'Flush':
+              this.setState({money: currentMoney + 900});
+              break;
+          case 'Four of a kind':
+              this.setState({money: currentMoney + 700});
+              break;
+          case 'Full house':
+              this.setState({money: currentMoney + 500});
+              break;
+          case 'Straight':
+              this.setState({money: currentMoney + 300});
+              break;
+          case 'Three of a kind':
+              this.setState({money: currentMoney + 150});
+              break;
+          case 'Two pairs':
+              this.setState({money: currentMoney + 100});
+              break;
+          case 'One pair':
+              this.setState({money: currentMoney + 50});
+              break;
+          default:
+              break;
+      }
+  };
 
   render() {
     const fiveCards = this.state.fiveCards.map((card, index) => (
@@ -37,6 +74,7 @@ class App extends Component {
           <div><button onClick={this.shuffleCards}>Shuffle Cards</button></div>
           <p>{this.state.result}</p>
           {fiveCards}
+          <p>Your money: {this.state.money}</p>
       </div>
     );
   }
